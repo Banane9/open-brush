e873b106dc7bec10af9d278fb42274d62366b912
Arne Kiesewetter <banane9@live.de>
2023-07-07 17:41:55 +0200

    Add tag for brushes used in moodworlds

--- Assets/Scripts/Brushes/BrushTag.cs
+++ Assets/Scripts/Brushes/BrushTag.cs
@@ -7,24 +7,36 @@ using UnityEngine.Localization;
 
 namespace TiltBrush
 {
+    /// <summary>
+    /// Create as Data Assets in Assets/Resources/Brushes/Tags to assign tags to <see cref="BrushDescriptor"/>s.<br/>
+    /// The BrushTagger (Open Brush > Rewrite Brush Tags) assigns them to the descriptors.
+    /// </summary>
     [CreateAssetMenu(fileName = "Tag", menuName = "Brush Tag")]
     public class BrushTag : ScriptableObject
     {
-        [field: SerializeField]
-        [Tooltip("The display name of the tag.")]
-        public string DisplayName { get; private set; }
-
-        [field: SerializeField]
-        [Tooltip("The internally used version of the tag. Should be safe for cfg files.")]
-        public string DurableName { get; private set; }
-
         [SerializeField]
-        [DisplayName("Brushes")]
         [Tooltip("The brushes this tag gets assigned to.")]
         private BrushDescriptor[] brushes;
 
+        /// <summary>
+        /// Gets the set of <see cref="BrushDescriptor"/>s that this tag should be assigned to.
+        /// </summary>
         public HashSet<BrushDescriptor> Brushes { get; private set; }
 
-        private void OnEnable() => Brushes = new(brushes);
+        /// <summary>
+        /// Gets this tag's display name.
+        /// </summary>
+        [field: SerializeField]
+        [field: Tooltip("The display name of the tag.")]
+        public string DisplayName { get; private set; }
+
+        /// <summary>
+        /// Gets this tag's internal and cfg-safe name.
+        /// </summary>
+        [field: SerializeField]
+        [field: Tooltip("The internally used version of the tag. Should be safe for cfg files.")]
+        public string DurableName { get; private set; }
+
+        private void OnEnable() => Brushes = new(brushes ?? Array.Empty<BrushDescriptor>());
     }
 }
